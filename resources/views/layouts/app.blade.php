6fc8ad47721295b2d438f1888c25cc16105464f3
milamocha <d00253640@student.dkit.ie>
2024-03-09 22:47:55 +0000

    put playlists page in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,7 @@
                 <nav class="space-x-4 text-white text-sm sm:text-base">
                     <a class="no-underline hover:underline" href="/blog">Blog</a>
                     <a class="no-underline hover:underline" href="/songgen">Song Finder</a>
+                    <a class="no-underline hover:underline" href="/playlists">Playlists</a>
                     @guest
                         <a class="no-underline hover:underline" href="{{ route('login') }}">{{ __('Login') }}</a>
                         @if (Route::has('register'))
