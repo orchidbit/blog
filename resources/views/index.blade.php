7be817ef2f36ee28401be8258ce507707325f2c8
milamocha <d00253640@student.dkit.ie>
2024-03-10 19:14:57 +0000

    changed button gradient and on hover

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
                 </h1>
                 <a 
                     href="/blog"
-                    class="text-center bg-purple-400 text-gray-700 py-2 px-4 font-bold text-xl uppercase" style="border-radius: 10px">
+                    class="text-center bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 hover:from-pink-400 hover:to-blue-400 via purple-400 py-2 px-4 font-bold text-xl uppercase" style="border-radius: 10px">
                     Read More
                 </a>
             </div>
