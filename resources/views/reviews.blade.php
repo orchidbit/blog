2b12bb2c239563f78ad101ff7a7724024f4d0a19
milamocha <d00253640@student.dkit.ie>
2024-03-14 00:07:37 +0000

    reviews page completed

--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -1,15 +1,67 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-4/5 m-auto text-center">
-    <div class="py-15 border-b border-gray-200">
-        <h1 class="text-6xl">
-            Reviews
-        </h1>
+<div class="songgencontainer">
+    <div class="greytitle2">Reviews</div>
+    <p style="color: #4B5563;">My reviews and ratings of songs</p><br>
+<div>
+
+<div class="reviewscontainer">
+    <div class="reviewcard">
+        <img src="/images/moonlightkali.png">
+        <div class="reviewtitle">
+            <h5>Moonlight</h5>
+            <p>Kali Uchis</p>
+        </div>
+        <div>
+            <div style="font-size: 30px"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half"></i></div>
+            <div class="reviewtext">This song sounds ethereal and enchanting, as if I'm floating on fluffy clouds when listening to it.</div>
+        </div>
     </div>
-    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-        Rate music
-    </p>
+    <div class="reviewcard">
+        <img src="/images/3nights.jpg">
+        <div class="reviewtitle">
+            <h5>3 Nights</h5>
+            <p>Dominic Fike</p>
+        </div>
+        <div>
+            <div style="font-size: 30px"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+            <div class="reviewtext">Really happy upbeat vibes, great song that anyone would enjoy.</div>
+        </div>
+    </div>
+    <div class="reviewcard">
+        <img src="/images/thelessiknowthebetter.jpg">
+        <div class="reviewtitle">
+            <h5>The Less I Know The Better</h5>
+            <p>Tame Impale</p>
+        </div>
+        <div>
+            <div style="font-size: 30px"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+            <div class="reviewtext">Unique funky tune, although can be too similar and repititive throughout.</div>
+        </div>
+    </div>
+    <div class="reviewcard">
+        <img src="/images/sunflowerspider.jpg">
+        <div class="reviewtitle">
+            <h5>Sunflower</h5>
+            <p>Post Malone, Swae Lee</p>
+        </div>
+        <div>
+            <div style="font-size: 30px"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half"></i></div>
+            <div class="reviewtext">Such an amazing song. Reminds me of the summer. Took me a few listens to really start enjoying it.</div>
+        </div>
+    </div>
+    <div class="reviewcard">
+        <img src="/images/colorviolet.jpg">
+        <div class="reviewtitle">
+            <h5>The Color Violet</h5>
+            <p>Tory Lanez</p>
+        </div>
+        <div>
+            <div style="font-size: 30px"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+            <div class="reviewtext">This song was like love at first sight upon first hearing it. It still sounds perfect many months later.</div>
+        </div>
+    </div>
 </div>
 
 <br><br><br>
